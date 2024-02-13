91136b8c37fdf3dff7b056a4e99ce2082eda3a82
Akash Kumar <meakash7902@gmail.com>
2024-02-13 21:04:17 +0530

    test
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1218,9 +1218,7 @@ func (c *KpmClient) downloadDeps(deps pkg.Dependencies, lockDeps pkg.Dependencie
 		source := fmt.Sprintf("%s@%s", d.Name, d.Version)
 		err = depGraph.AddVertex(source)
 		if err != nil {
-			if err != graph.ErrVertexAlreadyExists {
-				return nil, nil, err
-			}
+			return nil, nil, err
 		}
 
 		sourcesOfNestedDepGraph, err := FindSource(nestedDepGraph)
@@ -1228,7 +1226,7 @@ func (c *KpmClient) downloadDeps(deps pkg.Dependencies, lockDeps pkg.Dependencie
 			return nil, nil, err
 		}
 
-		depGraph, err = graph.Union(depGraph, nestedDepGraph)
+		depGraph, err = Union(depGraph, nestedDepGraph)
 		if err != nil {
 			return nil, nil, err
 		}
@@ -1340,4 +1338,62 @@ func FindSource[K comparable, T any](g graph.Graph[K, T]) ([]K, error) {
 		}
 	}
 	return sources, nil
+}
+
+// Union combines two given graphs into a new graph. The vertex hashes in both
+// graphs are expected to be unique. The two input graphs will remain unchanged.
+//
+// Both graphs should be either directed or undirected. All traits for the new
+// graph will be derived from g.
+// 
+// If the same vertex happens to be in both g and h, then an error will not be 
+// thrown as happens in original Union function and successful operation takes place.
+func Union[K comparable, T any](g, h graph.Graph[K, T]) (graph.Graph[K, T], error) {
+	union, err := g.Clone()
+	if err != nil {
+		return union, fmt.Errorf("failed to clone g: %w", err)
+	}
+
+	adjacencyMap, err := h.AdjacencyMap()
+	if err != nil {
+		return union, fmt.Errorf("failed to get adjacency map: %w", err)
+	}
+
+	addedEdges := make(map[K]map[K]struct{})
+
+	for currentHash := range adjacencyMap {
+		vertex, err := h.Vertex(currentHash)
+		if err != nil {
+			return union, fmt.Errorf("failed to get vertex %v: %w", currentHash, err)
+		}
+
+		err = union.AddVertex(vertex)
+		if err != nil {
+			return union, fmt.Errorf("failed to add vertex %v: %w", currentHash, err)
+		}
+	}
+
+	for _, adjacencies := range adjacencyMap {
+		for _, edge := range adjacencies {
+			if _, sourceOK := addedEdges[edge.Source]; sourceOK {
+				if _, targetOK := addedEdges[edge.Source][edge.Target]; targetOK {
+					// If the edge addedEdges[source][target] exists, the edge
+					// has already been created and thus can be skipped here.
+					continue
+				}
+			}
+
+			err = union.AddEdge(edge.Source, edge.Target)
+			if err != nil && err != graph.ErrEdgeAlreadyExists {
+				return union, fmt.Errorf("failed to add edge (%v, %v): %w", edge.Source, edge.Target, err)
+			}
+
+			if _, ok := addedEdges[edge.Source]; !ok {
+				addedEdges[edge.Source] = make(map[K]struct{})
+			}
+			addedEdges[edge.Source][edge.Target] = struct{}{}
+		}
+	}
+
+	return union, nil
 }
